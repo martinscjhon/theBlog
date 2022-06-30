f378ca35e13d2bc133fd169bb878d73eb00eac34
Jhonatan Martins <j.martins@vertex.vertexdigital.co>
2022-06-30 15:40:54 -0300

    criando header

---
+++ src/components/Header.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="header">
+    <header>
+      <div>
+        <img src="" alt="" />
+      </div>
+
+      <nav>
+        <ul>
+          <li><a href="">Home</a></li>
+          <li><a href="">Sobre</a></li>
+          <li><a href="">Categorias</a></li>
+          <li><a href="">Contato</a></li>
+        </ul>
+      </nav>
+
+      <div>
+        <input />
+        <button>
+          <img src="" alt="" />
+        </button>
+      </div>
+
+    </header>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeaderComponent',
+  data() {
+    return {
+      images:
+        {
+          logo: 'src/assets/images/logo.svg',
+          alt: 'Logo da imagem',
+        },
+
+    };
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
