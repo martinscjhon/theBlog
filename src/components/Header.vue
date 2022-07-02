450c5771e1e8ae4b4a4e5e4f947dc253f16a3bfb
Jhonatan Martins <j.martins@vertex.vertexdigital.co>
2022-07-02 13:26:46 -0300

    estrutura do header finalizado

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,46 +1,70 @@
 <template>
-  <div class="header">
+  <div id="header">
     <header>
       <div>
-        <img src="" alt="" />
+        <img :src="images[0].logo" :alt="images[0].alt" />
       </div>
 
       <nav>
         <ul>
-          <li><a href="">Home</a></li>
-          <li><a href="">Sobre</a></li>
-          <li><a href="">Categorias</a></li>
-          <li><a href="">Contato</a></li>
+          <li><a href="">{{links.name.home}}</a></li>
+          <li><a href="">{{links.name.aboutUs}}</a></li>
+          <li><a href="">{{links.name.category}}</a></li>
+          <li><a href="">{{links.name.contact}}</a></li>
         </ul>
       </nav>
 
-      <div>
-        <input />
+      <div class="search">
+        <label
+          for="input-search"
+          class="sr-only"
+          >
+            Buscar
+          </label>
+        <input
+          type="text"
+          placeholder="Buscar"
+          id="input-search"
+        />
         <button>
-          <img src="" alt="" />
+          <img :src="images[1].icon" :alt="images[1].alt" />
         </button>
       </div>
-
     </header>
   </div>
 </template>
 
 <script>
+
 export default {
   name: 'HeaderComponent',
   data() {
     return {
-      images:
+      images: [
         {
-          logo: 'src/assets/images/logo.svg',
-          alt: 'Logo da imagem',
+          // eslint-disable-next-line global-require
+          logo: require('../assets/images/logo.svg'),
+          alt: 'logo-the-blog-header',
         },
-
+        {
+          // eslint-disable-next-line global-require
+          icon: require('../assets/images/search.svg'),
+          alt: 'icon-serach-header',
+        },
+      ],
+      links: {
+        name: {
+          home: 'Home',
+          aboutUs: 'Sobre',
+          category: 'Categorias',
+          contact: 'Contato',
+        },
+      },
     };
   },
 };
 </script>
 
 <style scoped>
-
+  @import url("../styles/header.css")
 </style>
