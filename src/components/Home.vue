257a10c8c104959c0af1d498f573668f878f28e8
Jhonatan Martins <j.martins@vertex.vertexdigital.co>
2022-07-02 13:39:12 -0300

    criando component home

---
+++ src/components/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="home">
+    <section class="content">
+      <div class="text">
+        <h1></h1>
+        <p></p>
+
+        <div class="footer-content">
+          <p></p>
+          <img src="" alt="">
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeComponent',
+  data() {
+    return {
+    };
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
