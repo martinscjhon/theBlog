73ee93e09b95bac88548cacb65cd2c307541dd75
Jhonatan Martins <j.martins@vertex.vertexdigital.co>
2022-07-04 11:09:47 -0300

    Criando a Home do projeto
    ```
    - Desenvolvendo a home do projeto.
    On branch master
    Changes to be committed:
    	modified:   public/styles/global.css
    	modified:   src/App.vue
    	modified:   src/components/Home.vue
    	modified:   src/styles/header.css
    	new file:   src/styles/home.css
    
    ```

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     <HeaderComponent />
+    <HomeComponent />
   </div>
 </template>
 
 <script>
 import HeaderComponent from './components/Header.vue';
+import HomeComponent from './components/Home.vue';
 
 export default {
 
   name: 'App',
   components: {
     HeaderComponent,
+    HomeComponent,
   },
 };
 </script>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,14 +2,22 @@
   <div id="home">
     <section class="content">
       <div class="text">
-        <h1></h1>
-        <p></p>
+        <div>
+          <h1>{{title}}</h1>
+          <p>{{subTitle}}</p>
 
-        <div class="footer-content">
-          <p></p>
-          <img src="" alt="">
+          <a href="">
+            <div>
+              <h3></h3>
+              <img :src="images[0].arrow" :alt="images[0].alt" />
+            </div>
+          </a>
         </div>
       </div>
+
+        <div class="image-content">
+          <img :src="images[1].post" :alt="images[1].alt">
+        </div>
     </section>
   </div>
 </template>
@@ -19,11 +27,25 @@ export default {
   name: 'HomeComponent',
   data() {
     return {
+      title: 'Veja o guia definitivo para conquistar seus objetivos como DEV em 2022',
+      subTitle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nibh nibh eu in aliquet ut adipiscing neque. Sed volutpat aenean sit vitae, sed tristique placerat hac.',
+      images: [
+        {
+          // eslint-disable-next-line global-require
+          arrow: require('../assets/images/arrow-right.svg'),
+          alt: 'imagem da seta saiba mais',
+        },
+        {
+          // eslint-disable-next-line global-require
+          post: require('../assets/images/featured-image.png'),
+          alt: 'imagem do post-1',
+        },
+      ],
     };
   },
 };
 </script>
 
 <style scoped>
-
+  @import url("../styles/home.css")
 </style>
